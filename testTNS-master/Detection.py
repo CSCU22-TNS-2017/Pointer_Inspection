ee7f1825089a5cf5db113ec3c2f072cc71b248eb
donnapatt <eaimac128@gmail.com>
2017-06-23 10:54:43 +0700

    update 23/6/2017
    เริ่มทำbending
    1.เพิ่มรูปถ่าย
    2.เพิ่มเติมโค้ดเช็คการbending
    โดยมีเงื่อนไขว่่ากล้องกับตัวชิ้นงานห่างกัน 19ซม. ในระดับเดียวกัน
    เช็คผ่านthresholdค่าที่ใช้อยู่ในโค้ด
    
    จากระยยะดังกล่าวจะได้ว่า 1pixel = 0.07 ซม.

---
+++ testTNS-master/Detection.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import copy
+
+# function gamma correction
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+    for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+def count_mat(m,label):
+    max_i,max_j = m.shape
+    ans = []
+    for i in range(label):
+        ans.append(0)
+    for i in range(0,max_i):
+        bnc = (np.bincount(m[i][0:max_j])).tolist()
+        if (len(bnc)<label):
+            for i in range(label-len(bnc)):
+                bnc.append(0)
+        ans = add(ans,bnc)
+    return ans
+
+def add(ans,bnc):
+    return [ans[i]+bnc[i] for i in range(len(ans))]
+
+def write_circle(output,ori):
+    cnt = count_mat(output[1], output[0])
+    cnt2 = copy.copy(cnt)
+    cnt2.remove(max(cnt))
+    ind = []
+    ind.append(cnt.index(max(cnt)))
+    ind.append(cnt.index(max(cnt2)))
+
+    print('ind', ind)
+    print('stat', output[2])
+    print('centroid', output[3])
+    for i in output[3]:
+        if (output[3].tolist()).index(i.tolist()) not in ind:
+            cv2.circle(ori, (int(i[0]), int(i[1])), 20, (0, 255, 0), 2)
+
+def multi_thresh(img,low,mid,fst,sec,thd):
+    height,width = img.shape[:2]
+    ret = copy.copy(img)
+    for i in range(height):
+        for j in range(width):
+            if img[i][j] <= low:
+                ret[i][j] = fst
+            elif img[i][j]>low and img[i][j]<=mid:
+                ret[i][j] = sec
+            else:
+                ret[i][j] = thd
+    return ret
--- testTNS-master/connectCAM.py
+++ testTNS-master/connectCAM.py
@@ -1,21 +1,82 @@
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as pimg
+import copy as cp
+import time as t
 import os
+import numpy as np
+
+def write_line(img):
+    cv2.line(img, (0, 160), (640, 160), (0, 0, 255), 1)
+    cv2.line(img, (0, 165), (640, 165), (0, 255, 0), 1)
+    cv2.line(img, (0, 170), (640, 170), (0, 0, 255), 1)
+    cv2.line(img, (0, 240), (640, 240), (255, 255, 255), 1)
+    # cv2.line(orig, (0, 310), (640, 310), (0, 0, 255), 1)
+    cv2.line(img, (0, 315), (640, 315), (0, 255, 0), 1)
+
+def find_pos(img):
+    height,width = img.shape
+    order = []
+    for i in range(width):
+        for j in range(height):
+            if img[j][i]==0:
+                a = (j,i)
+                order.append(a)
+                break
+            continue
+
+    return order
+
 
 cap = cv2.VideoCapture(0)
-# os.system('C:\\Users\\DP\\Desktop\\LED_off.bat')
-n=8
+t.sleep(5)
+os.system('C:\\Users\\DP\\Desktop\\LED_off.bat')
+n=22
 while True:
     ret, orig = cap.read()
-    copy1 = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
-    ret, thresh1 = cv2.threshold(copy1, 169, 255, cv2.THRESH_BINARY)
+    img = cp.copy(orig)
+    copy1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(copy1, 80, 255, cv2.THRESH_BINARY)
     cv2.imshow('thres', thresh1)
-    cv2.imshow('orig', orig)
-    if cv2.waitKey(1) & 0xFF == ord('c'):
-        print(n)
-        cv2.imwrite(str(n)+'.jpg', orig)
-        n+=1
-    if cv2.waitKey(1) & 0xFF == ord('q'):#or (move_x=='Ok' and move_y == 'Ok'):
+    write_line(img)
+    lapla = cv2.Laplacian(thresh1, cv2.CV_64F)
+    sobely = cv2.Sobel(thresh1, cv2.CV_64F, 0, 1, ksize=1)
+    sobely+=255
+    sobely = np.uint8(sobely)
+    plt.imshow(sobely,cmap='gray')
+    plt.show()
+    # area = thresh1[0:160,0:640]
+    # boo = 0 in area
+    # # cv2.line(orig, (0, 320), (640, 320), (0, 0, 255), 1)
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    # if boo:
+    #     cv2.putText(img, 'Ng', (10, 350), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    # else:
+    #     cv2.putText(img, 'good', (10, 350), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    allpoint = (find_pos(sobely))
+    firstpoint = allpoint[0]
+    midpoint = allpoint[int(len(allpoint)/2)]
+    lastpoint = allpoint[len(allpoint)-1]
+    # plt.imshow(thresh1,cmap='gray')
+    # plt.show()
+    # cv2.line(img, (midpoint[1],midpoint[0]), (lastpoint[1],lastpoint[0]), (0, 0, 255), 1)
+    cv2.circle(img, (firstpoint[1], firstpoint[0]), 20, (0, 0, 255), 2)
+    cv2.circle(img, (lastpoint[1],lastpoint[0]), 20, (0, 0, 255), 2)
+    # plt.imshow(sobely,cmap='gray')
+    # plt.show()
+    cv2.imshow('orig', img)
+    cv2.imshow('sobelY', sobely)
+    # cv2.imshow('area',area)
+    # if cv2.waitKey(1) & 0xFF == ord('c'):
+    #     print(n)
+    #     cv2.imwrite(str(n)+'.png', orig)
+    #     n+=1
+    if cv2.waitKey(1) and 0xFF == ord('q'):#or (move_x=='Ok' and move_y == 'Ok'):
         # cv2.imwrite('4.jpg', orig)
         break
+# img = pimg.imread('22.png')
+# plt.imshow(img)
+# plt.show()
 cap.release()
 cv2.destroyAllWindows()
